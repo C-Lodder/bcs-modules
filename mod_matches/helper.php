658383f9e049b42645b4d48c5d6711aa1f4bb91d
C-Lodder <lodder15@hotmail.co.uk>
2018-03-16 21:46:28 +0000

    Rewrite mod_matchs

--- mod_matches/helper.php
+++ mod_matches/helper.php
@@ -10,38 +10,18 @@ defined('_JEXEC') or die('Restricted access');
 
 class ModMatchesHelper
 {
-	public function getMatchStandings()
+	public function groupByKey($array) 
 	{
-		$db = JFactory::getDbo();
+		$result = [];
+		$array  = json_decode($array, true);
 
-		$query = $db->getQuery(true)->select(['a.MapId', 'a.Id', 'a.PlayerId', 'a.Score', 'b.Id', 'b.Name', 'b.Author', 'c.Id', 'c.NickName'])
-			->from($db->qn('match_records', 'a'))
-			->join('INNER', $db->qn('match_maps', 'b') . ' ON (' . $db->qn('a.MapId') . ' = ' . $db->qn('b.Id') . ')')
-			->join('INNER', $db->qn('match_players', 'c') . ' ON (' . $db->qn('a.PlayerId') . ' = ' . $db->qn('c.Id') . ')')
-			->order($db->qn('a.Score') . ' ASC');
-		$db->setQuery($query);
-
-		$results = $db->loadAssocList();
-		
-		if ($db->getErrorNum())
+		foreach ($array as $sub) 
 		{
-			throw new RuntimeException($db->getErrorMsg(), $db->getErrorNum());
+			foreach ($sub as $k => $v) 
+			{
+				$result[$k][] = $v;
+			}
 		}
-
-		$grouped = $this->groupAssoc($results, 'MapId');
-
-		return $grouped;
-	}
-
-	public function groupAssoc($array, $key)
-	{
-		$return = [];
-
-		foreach($array as $v)
-		{
-			$return[$v[$key]][] = $v;
-		}
-
-		return $return;
+		return $result;
 	}
 }
--- mod_matches/tmpl/default.php
+++ mod_matches/tmpl/default.php
@@ -8,126 +8,27 @@
 
 defined('_JEXEC') or die('Restricted access');
 
-JFactory::getDocument()->addStyleDeclaration('
-	.match_box {
-		padding: 10px;
-		background: rgba(0,0,0,.3);
-	}
-	.match_number {
-		display: inline-block;
-		min-width: 20px;
-		text-align: right;
-		color: #fff;
-	}
-	.top3 {
-		color: #76ec00;
-	}
-	.top6 {
-		color: #ff0;
-	}
-	.top10 {
-		color: #e42222;
-	}
-');
 ?>
 
-<div id="scores" class="uk-text-center uk-text-bold">
-	<h1 class="uk-article-title uk-margin-top-remove">Standings</h1>
-	<p class="uk-h3 uk-margin-top-remove">BCS (<span class="scoreBCS"></span>)<span class="uk-h4 uk-text-muted uk-display-inline-block uk-margin-small-left uk-margin-small-right">vs</span>Speed (<span class="scoreOther"></span>)</p>
+<div class="uk-overflow-container" style="height:340px">
+	<table class="uk-table uk-table-striped">
+		<thead>
+			<tr>
+				<th>Title</th>
+				<th>Date</th>
+				<th>Score</th>
+				<th class="uk-hidden-small">Winner</th>
+			</tr>
+		</thead>
+		<tbody>
+		<?php foreach ($matches as $match) : ?>
+			<tr>
+				<td>BCS <span class="uk-text-warning">vs</span> TFC</td>
+				<td>27th November 2017</td>
+				<td class="uk-text-success">464 : 86</td>
+				<td class="uk-hidden-small">BCS</td>
+			</tr>
+		<?php endforeach; ?>
+		</tbody>
+	</table>
 </div>
-
-<div class="uk-grid uk-grid-small" data-uk-grid-margin>
-	<?php 
-		foreach ($standings as $map)
-		{
-			echo '<div class="uk-width-medium-1-3">';
-			echo '<div class="match_box">';
-			echo '<ul class="uk-list uk-margin-bottom">';
-			echo '<li class="uk-h6"><span class="uk-icon-justify uk-icon-flag-checkered" aria-hidden="true"></span> ' . $cp->toHTML($map[0]['Name']) . '</li>';
-			echo '</ul>';
-			echo '<ul class="uk-list uk-list-line">';
-			$i = 1;
-			$points = 10;
-			foreach ($map as $v)
-			{
-				if ($i <= 10)
-				{
-					$class = 'top10';
-
-					if ($i <= 3)
-					{
-						$class = 'top3';
-					}
-					else if ($i <= 6)
-					{
-						$class = 'top6';
-					}
-
-					// Strip tags and check for 'вcs' in name
-					$bcs = '';
-					$nickname = $cp->toHTML($v['NickName']);
-					if ((strpos(strip_tags($nickname), '|') !== false) && (strpos(strip_tags($nickname), 'bcs') !== false))
-					{
-						$bcs = 'bcs';
-					}
-
-					$input = $v['Score'];
-
-					$microseconds = ($input % 1000 < 100) ? 0 . $input % 1000 : $input % 1000;
-					$microseconds = ($microseconds < 10) ? 0 . $microseconds : $microseconds;
-					$input = floor($input / 1000);
-
-					$seconds = ($input % 60 < 10) ? 0 . $input % 60 : $input % 60;
-					$input = floor($input / 60);
-
-					$minutes = ($input % 60 == 0) ? '' : $input % 60 . '.';
-					$input = floor($input / 60);
-					
-					$time = $minutes . $seconds . '.' . $microseconds;
-
-					echo '<li class="getPoints" data-name="' . $bcs . '" data-points="' . $points-- . '">';
-					echo '<span class="match_number">'. $i++ . '.</span>';
-					echo '<span class="' . $class . ' uk-text-bold">' . $time . '</span>';
-					echo ' - ' . $nickname;
-					echo '</li>';
-				}
-			}
-			echo '</ul>';
-			echo '</div>';
-			echo '</div>';
-		}
-	?>
-</div>
-
-<script>
-document.addEventListener('DOMContentLoaded', function() {
-
-	var totalBCS   = 0,
-		totalOther = 0,
-		wrapper    = document.getElementById('scores'),
-		points     = document.querySelectorAll('.getPoints');
-
-	for (var i = 0; i < points.length; i++)
-	{
-		var el = points[i];
-
-		if (el.getAttribute('data-name') === 'bcs')
-		{
-			totalBCS += parseInt(el.getAttribute('data-points'));
-		}
-		else
-		{
-			totalOther += parseInt(el.getAttribute('data-points'));
-		}
-	}
-
-	var classBCS   = totalBCS > totalOther ? 'success' : 'danger',
-		classOther = totalBCS > totalOther ? 'danger' : 'success';
-
-	wrapper.querySelector('.scoreBCS').innerHTML = totalBCS;
-	wrapper.querySelector('.scoreBCS').classList.add('uk-text-' + classBCS);
-
-	wrapper.querySelector('.scoreOther').innerHTML = totalOther;
-	wrapper.querySelector('.scoreOther').classList.add('uk-text-' + classOther);
-});
-</script>

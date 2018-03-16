df212dc054bb8efff1b217032a24483a57896459
C-Lodder <lodder15@hotmail.co.uk>
2018-03-16 20:50:53 +0000

    Use namespaced classes for mod_servers

--- mod_servers/mod_servers.php
+++ mod_servers/mod_servers.php
@@ -8,16 +8,19 @@
 
 defined('_JEXEC') or die('Restricted access');
 
-// Get the helper file
-JLoader::register('ModServersHelper', __DIR__ . '/helper.php');
-JLoader::register('TMFColorParser', JPATH_BASE . '/tmfcolorparser.php');
+use Joomla\CMS\Factory;
+use Joomla\CMS\Helper\ModuleHelper;
 
-$app 	= JFactory::getApplication();
+// Get the helper file
+\JLoader::register('ModServersHelper', __DIR__ . '/helper.php');
+\JLoader::register('TMFColorParser', JPATH_BASE . '/tmfcolorparser.php');
+
+$app 	= Factory::getApplication();
 $active = $app->getMenu()->getActive();
 $Itemid = is_null($active) ? null : $active->id;
 $title 	= $module->title;
 
-$user = JFactory::getUser();
+$user = Factory::getUser();
 
 $helper = new ModServersHelper;
 $cp 	= new TMFColorParser();
@@ -25,4 +28,4 @@ $cp->replaceHex('#eeeeee', '#333333');
 
 $servers = $helper->getServers();
 
-require JModuleHelper::getLayoutPath('mod_servers');
+require ModuleHelper::getLayoutPath('mod_servers');

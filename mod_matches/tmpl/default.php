2146b8d45a2b3f4704aa2a9e2dc5b286fb3af6a5
C-Lodder <lodder15@hotmail.co.uk>
2018-04-20 12:03:05 +0100

    Simplify detection of winner

--- mod_matches/tmpl/default.php
+++ mod_matches/tmpl/default.php
@@ -22,11 +22,26 @@ defined('_JEXEC') or die('Restricted access');
 		</thead>
 		<tbody>
 		<?php foreach ($matches as $match) : ?>
+			<?php
+				$winner = $match[0];
+				$textClass = 'uk-text-danger';
+
+				if ($match[2] > $match[3])
+				{
+					$winner = 'BCS';
+					$textClass = 'uk-text-success';
+				}
+				else if ($match[2] === $match[3])
+				{
+					$winner = 'Draw';
+					$textClass = 'uk-text-warning';
+				}
+			?>
 			<tr>
 				<td>BCS <span class="uk-text-warning">vs</span> <?php echo $match[0]; ?></td>
 				<td><?php echo $match[1]; ?></td>
-				<td class="uk-text-success"><?php echo $match[2]; ?> : <?php echo $match[3]; ?></td>
-				<td class="uk-hidden-small"><?php echo $match[4]; ?></td>
+				<td class="<?php echo $textClass; ?>"><?php echo $match[2]; ?> : <?php echo $match[3]; ?></td>
+				<td class="uk-hidden-small"><?php echo $winner; ?></td>
 			</tr>
 		<?php endforeach; ?>
 		</tbody>

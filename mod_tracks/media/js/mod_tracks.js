3713e0707ac72a2393ac3c5b6ff42aba6b08773e
C-Lodder <lodder15@hotmail.co.uk>
2022-02-18 12:17:35 +0000

    Add prev/next triggers to mod_tracks + JS improvements

--- mod_tracks/media/js/mod_tracks.js
+++ mod_tracks/media/js/mod_tracks.js
@@ -6,97 +6,104 @@
  */
 
 (() => {
-  // Assemble variables to submit
+  let count
+  let inView
+  const href = 'track'
   const options = Joomla.getOptions('bcs-tracks')
-  const wrapper = document.getElementById('bcstracks-slider')
-  const request = {
-    authors: options.authors.split(','),
+  const slider = document.getElementById('bcstracks-slider')
+  const triggers = document.querySelectorAll('#bcstracks [data-slide-move]')
+
+  const isScrolledIntoView = (el) => {
+    return parseInt(el.offsetLeft + 20, 10) <= slider.offsetWidth
+  }
+
+  const updateHrefs = (isNext) => {
+    triggers.forEach(item => {
+      const currentNumber = parseInt(item.getAttribute('href').replace(/\#track/, ''), 10)
+      const newNumber = isNext ? parseInt(currentNumber - 1, 10) : parseInt(currentNumber + 1, 10)
+      const movingForward = item.getAttribute('data-slide-move') === 'next'
+      let proceed = true
+
+      // Next button clicked
+      if ((movingForward && newNumber < 1) || (!movingForward && newNumber < (inView + 1))) {
+        proceed = false
+      }
+
+      // Prev button clicked
+      if ((!movingForward && newNumber > count) || (movingForward && newNumber > (count - inView))) {
+        proceed = false
+      }
+
+      if (proceed) {
+        item.setAttribute('href', `#${href + newNumber}`)
+      }
+    })
   }
 
   fetch('index.php?option=com_ajax&module=tracks&method=getAuthorTracks&format=json', {
     method: 'POST',
-    body: JSON.stringify(request)
+    body: JSON.stringify({ authors: options.authors.split(',') })
   })
   .then(response => response.json())
   .then(response => {
     const data = response.data
+    const keys = Object.keys(data)
+    count = keys.length
 
-    Object.keys(data).forEach(key => {
+    keys.forEach((key, index) => {
       const obj = data[key]
       const list = document.createElement('div')
+      list.id = href + (index + 1)
 
-      const anchor = document.createElement('a')
-      anchor.setAttribute('href', `${obj.endpoint}/maps/${obj.TrackID}`)
-      anchor.setAttribute('target', '_blank')
+      const mxLogoSrc = obj.TitlePack === 'Trackmania' ? 'Trackmania' : 'TMStadium'
+      const html = 
+      `<a href="${obj.endpoint}/maps/${obj.TrackID}" target="_blank" rel="noopener">
+        <img class="screenshot" src="${obj.screenshot}" alt="Thumbnail">
+        <span class="flex align-items-center flex-wrap">
+          <img class="mx-logo" src="media/mod_tracks/img/${mxLogoSrc}.webp" alt="MX Logo">
+          ${obj.GbxMapName}
+        </span>
+      </a>
+      <div class="by">by ${obj.Username}</div>
+      <div class="by">Uploaded ${obj.UploadedAt}</div>
+      <div class="flex flex-wrap justify-content-between">
+        <a class="btn btn-sm" href="${obj.endpoint}/maps/download/${obj.TrackID}" target="_blank" rel="noopener">Download</a>
+        <a class="btn btn-sm" href="${obj.endpoint}/maps/installandplay/${obj.TrackID}" target="_blank" rel="noopener">Install & Play</a>
+      </div>`
+      list.innerHTML = html
 
-      const name = document.createElement('span')
-      name.classList.add('flex', 'align-items-center', 'flex-wrap')
-      name.innerHTML = obj.GbxMapName
-
-      const mxLogo = new Image
-      const mxLogoSrc = obj.TitlePack === 'Trackmania' ? 'Trackmania' : 'TMStadium';
-      mxLogo.src = `media/mod_tracks/img/${mxLogoSrc}.webp`
-      mxLogo.classList.add('mx-logo')
-      mxLogo.addEventListener('load', () => {
-        name.prepend(mxLogo)
-      }, false)
-
-      const by = document.createElement('div')
-      by.classList.add('by')
-      by.innerHTML = `by ${obj.Username}`
-
-      const by2 = document.createElement('div')
-      by2.classList.add('by')
-      by2.innerText = `Uploaded ${obj.UploadedAt}`
-
-      const img = new Image
-      img.src = obj.screenshot
-      img.classList.add('screenshot')
-      img.addEventListener('load', () => {
-        anchor.prepend(img)
-      }, false)
-
-      anchor.append(name)
-
-      const actions = document.createElement('div')
-      actions.classList.add('flex', 'flex-wrap', 'justify-content-between')
-
-      const download = document.createElement('a')
-      download.classList.add('btn', 'btn-sm')
-      download.setAttribute('href', `${obj.endpoint}/maps/download/${obj.TrackID}`)
-      download.innerText = 'Download'
-
-      const install = document.createElement('a')
-      install.classList.add('btn', 'btn-sm')
-      install.setAttribute('href', `${obj.endpoint}/maps/installandplay/${obj.TrackID}`)
-      install.innerText = 'Install & Play'
-
-      actions.append(download)
-      actions.append(install)
-
-      list.append(anchor)
-      list.append(by)
-      list.append(by2)
-
-      // if (obj.magnets) {
-        // const magnet = document.createElement('div')
-        // magnet.classList.add('magnet')
-        // magnet.innerHTML = '<strong>WARNING</strong>: Contains magnetic blocks!'
-        // list.append(magnet)
-      // }
-
-      list.append(actions)
-
-      wrapper.insertBefore(list, wrapper.firstChild)
-
-      const height = wrapper.querySelector('div').height
-      wrapper.style.minHeight = `${height}px`
+      slider.insertBefore(list, slider.firstChild)
     })
   })
   .then(() => {
-    const placeholders = wrapper.getElementsByClassName('placeholder')
+    const placeholders = slider.getElementsByClassName('placeholder')
     while (placeholders.length > 0) {
       placeholders[0].parentNode.removeChild(placeholders[0])
     }
+  })
+  .then(() => {
+    inView = 1
+
+    if (isScrolledIntoView(slider.children[1])) {
+      inView = 2
+    }
+    if (isScrolledIntoView(slider.children[3])) {
+      inView = 4
+    }
+
+    triggers.forEach(trigger => {
+      trigger.classList.remove('hidden')
+
+      const isNext = trigger.getAttribute('data-slide-move') === 'next'
+      if (isNext) {
+        trigger.href = `#${href + parseInt(count - (inView), 10)}`
+      } else {
+        trigger.href = `#${href + parseInt(count, 10)}`
+      }
+
+      trigger.addEventListener('click', ({ currentTarget }) => {
+        updateHrefs(currentTarget.getAttribute('data-slide-move') === 'next')
+      })
+    })
   })
 })()
--- mod_tracks/tmpl/default.php
+++ mod_tracks/tmpl/default.php
@@ -12,6 +12,7 @@ use Joomla\CMS\Factory;
 use Joomla\CMS\HTML\HTMLHelper;
 
 HTMLHelper::_('script', 'mod_tracks/mod_tracks.min.js', ['version' => 'auto', 'relative' => true], ['type' => 'module']);
+// HTMLHelper::_('stylesheet', 'mod_tracks/mod_tracks.min.css', ['version' => 'auto', 'relative' => true]);
 
 $wa = Factory::getApplication()->getDocument()->getWebAssetManager();
 
@@ -26,53 +27,57 @@ catch (Exception $e)
 }
 ?>
 
-<div id="bcstracks-slider" class="bcstracks">
-	<div class="placeholder">
-		<div class="list-inner flex flex-middle flex-center">
-			<svg width="266px" height="193px"></svg>
+<div id="bcstracks" class="bcstracks">
+	<a href="#" data-slide-move="prev" class="hidden"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="var(--hiq-text-color)" d="M224 480c-8.188 0-16.38-3.125-22.62-9.375l-192-192c-12.5-12.5-12.5-32.75 0-45.25l192-192c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L77.25 256l169.4 169.4c12.5 12.5 12.5 32.75 0 45.25C240.4 476.9 232.2 480 224 480z"/></svg></a>
+	<a href="#" data-slide-move="next" class="hidden"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="var(--hiq-text-color)" d="M96 480c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L242.8 256L73.38 86.63c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l192 192c12.5 12.5 12.5 32.75 0 45.25l-192 192C112.4 476.9 104.2 480 96 480z"/></svg></a>
+	<div id="bcstracks-slider" class="bcstracks-slider">
+		<div class="placeholder">
+			<div class="list-inner flex flex-middle flex-center">
+				<svg width="266px" height="193px"></svg>
+			</div>
+			<div class="placeholder-content"></div>
+			<div class="placeholder-content"></div>
+			<div class="placeholder-content"></div>
+			<div class="flex flex-wrap justify-content-between">
+				<a class="button btn-sm" href="#">Download</a>
+				<a class="button btn-sm" href="#">Install &amp; Play</a>
+			</div>
 		</div>
-		<div class="placeholder-content"></div>
-		<div class="placeholder-content"></div>
-		<div class="placeholder-content"></div>
-		<div class="flex flex-wrap justify-content-between">
-			<a class="button btn-sm" href="#">Download</a>
-			<a class="button btn-sm" href="#">Install &amp; Play</a>
+		<div class="placeholder">
+			<div class="list-inner flex flex-middle flex-center">
+				<svg width="266px" height="193px"></svg>
+			</div>
+			<div class="placeholder-content"></div>
+			<div class="placeholder-content"></div>
+			<div class="placeholder-content"></div>
+			<div class="flex flex-wrap justify-content-between">
+				<a class="button btn-sm" href="#">Download</a>
+				<a class="button btn-sm" href="#">Install &amp; Play</a>
+			</div>
 		</div>
-	</div>
-	<div class="placeholder">
-		<div class="list-inner flex flex-middle flex-center">
-			<svg width="266px" height="193px"></svg>
+		<div class="placeholder">
+			<div class="list-inner flex flex-middle flex-center">
+				<svg width="266px" height="193px"></svg>
+			</div>
+			<div class="placeholder-content"></div>
+			<div class="placeholder-content"></div>
+			<div class="placeholder-content"></div>
+			<div class="flex flex-wrap justify-content-between">
+				<a class="button btn-sm" href="#">Download</a>
+				<a class="button btn-sm" href="#">Install &amp; Play</a>
+			</div>
 		</div>
-		<div class="placeholder-content"></div>
-		<div class="placeholder-content"></div>
-		<div class="placeholder-content"></div>
-		<div class="flex flex-wrap justify-content-between">
-			<a class="button btn-sm" href="#">Download</a>
-			<a class="button btn-sm" href="#">Install &amp; Play</a>
-		</div>
-	</div>
-	<div class="placeholder">
-		<div class="list-inner flex flex-middle flex-center">
-			<svg width="266px" height="193px"></svg>
-		</div>
-		<div class="placeholder-content"></div>
-		<div class="placeholder-content"></div>
-		<div class="placeholder-content"></div>
-		<div class="flex flex-wrap justify-content-between">
-			<a class="button btn-sm" href="#">Download</a>
-			<a class="button btn-sm" href="#">Install &amp; Play</a>
-		</div>
-	</div>
-	<div class="placeholder">
-		<div class="list-inner flex flex-middle flex-center">
-			<svg width="266px" height="193px"></svg>
-		</div>
-		<div class="placeholder-content"></div>
-		<div class="placeholder-content"></div>
-		<div class="placeholder-content"></div>
-		<div class="flex flex-wrap justify-content-between">
-			<a class="button btn-sm" href="#">Download</a>
-			<a class="button btn-sm" href="#">Install &amp; Play</a>
+		<div class="placeholder">
+			<div class="list-inner flex flex-middle flex-center">
+				<svg width="266px" height="193px"></svg>
+			</div>
+			<div class="placeholder-content"></div>
+			<div class="placeholder-content"></div>
+			<div class="placeholder-content"></div>
+			<div class="flex flex-wrap justify-content-between">
+				<a class="button btn-sm" href="#">Download</a>
+				<a class="button btn-sm" href="#">Install &amp; Play</a>
+			</div>
 		</div>
 	</div>
 </div>
@@ -84,4 +89,3 @@ catch (Exception $e)
 			'authors' => $params->get('authors'),
 		]
 	);
-?>
